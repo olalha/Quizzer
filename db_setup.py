087f166ab6a02fefcc4b0ddac2975b99f6672128
olalha <oahague@gmail.com>
2024-10-15 13:13:53 +0100

    db setup

---
+++ db_setup.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .utils import Base
+
+DATABASE_URL = 'sqlite:///quiz_generator.db'
+
+# Create a new engine instance and all tables
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
