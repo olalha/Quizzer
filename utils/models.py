325946212cd9fcdec927d14d56a58d26eafeb1c4
olalha <oahague@gmail.com>
2024-10-15 13:00:55 +0100

    database models

---
+++ utils/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, JSON
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+Base = declarative_base()
+
+# Table for uploaded files
+class UploadedFile(Base):
+    __tablename__ = 'uploaded_files'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    filename = Column(String, nullable=False)
+    filepath = Column(String, nullable=False)
+    upload_time = Column(DateTime, default=datetime.utcnow)
+    title = Column(Text)
+    description = Column(Text)
+
+# Table for quizzes
+class Quiz(Base):
+    __tablename__ = 'quizzes'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    file_id = Column(Integer, nullable=False)
+    quiz_content = Column(JSON, nullable=False)
+    created_time = Column(DateTime, default=datetime.utcnow)
+    title = Column(Text)
+    description = Column(Text)
